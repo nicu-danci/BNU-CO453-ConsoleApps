ef3bbef4505628c2d41f9a69362822eb7bc446e6
nicu-danci <92030368+nicu-danci@users.noreply.github.com>
2022-03-07 02:19:38 +0000

    Add files via upload

---
+++ ConsoleAppProject/App02/BMI_Stats.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleAppProject.App02
+{
+    /// <summary>
+    /// A list of WHO Weight Status 
+    /// </summary>
+    /// 
+    /// <author>
+    /// Nicoara Danci 07/03/2022
+    /// </author>
+    public enum BMI_Status
+    {
+        None,
+        [Display(Name = "Underweight")]
+        UnderWeight,
+        [Display(Name = "Normal weight")]
+        NormalWeight,
+        [Display(Name = "Overweight")]
+        OverWeight,
+        [Display(Name = "Obese Class I")]
+        ObeseI,
+        [Display(Name = "Obese Class II")]
+        ObeseII,
+        [Display(Name = "Obese Class III")]
+        ObeseIII
+    }
+}
